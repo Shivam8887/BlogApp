2121248b75b0c22a750221a8cef6f3d201905357
Shivam Pandey <shivam91business@gmail.com>
2025-04-22 06:21:09 +0530

    Update API

--- frontend/src/APi/api.jsx
+++ frontend/src/APi/api.jsx
@@ -1,8 +1,8 @@
-const host =import.meta.env.MODE === "development" ? "http://localhost:3000":"/";
-export const login = `${host}/login`;
-export const signup = `${host}/signup`;
-export const blogs = `${host}/blogs`;
-export const createpost = `${host}/createpost`;
-export const allpost = `${host}/allpost`
-export const deletepost = `${host}/deletepost`
-export const updatepost = `${host}/updatepost`
+const host =import.meta.env.MODE === "development" ? "http://localhost:3000":"https://blog-update-3-1.onrender.com/";
+export const login = `${host}login`;
+export const signup = `${host}signup`;
+export const blogs = `${host}blogs`;
+export const createpost = `${host}createpost`;
+export const allpost = `${host}allpost`
+export const deletepost = `${host}deletepost`
+export const updatepost = `${host}updatepost`
